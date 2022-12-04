a31028d6c12e8974e9db7247ebeffa29d3fcf0f3
youcef achira <115021298+salah-a11@users.noreply.github.com>
2022-12-04 02:47:26 +0100

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  emailAPI: 'http://XXXXXX.com/contact-form.php',
+  database: 'firebase',
+  social: {
+    role: 'Guest',
+    fblink: 'https://www.facebook.com/elishconsulting',
+    linkedin: 'https://www.linkedin.com/company/elish-consulting/about/?viewAsMember=true',
+    github: 'https://github.com/AmitXShukla',
+    emailid: 'info@elishconsulting.com'
+  },
+  socialAuthEnabled: true,
+  firebase: {
+    apiKey: "",
+    authDomain: "elishcrm.firebaseapp.com",
+    databaseURL: "https://elishcrm.firebaseio.com",
+    projectId: "elishcrm",
+    storageBucket: "elishcrm.appspot.com",
+    messagingSenderId: "",
+    appId: ""
+  }
+}
